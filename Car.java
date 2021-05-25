04c245da1619eb86c9efa2e97632e78ffe1410b9
tamitakada <tamitakada@gmail.com>
2021-05-25 14:10:13 -0400

    added Car class w basic properties & get methods

---
+++ Car.java
@@ -0,0 +1,48 @@
+public class Car {
+  //Car properties
+  private double power;
+  private double grip;
+  private double aero;
+  private double weight;
+
+  // private Gear gear;
+
+  //Car appearance
+  // private Livery livery;
+
+  //Car parts
+  // private Tire tire;
+  // private FrontWing frontWing;
+  // private RearWing rearWing;
+  // private Engine engine;
+  // private Suspension suspension;
+  // private Transmission transmission;
+
+  public void move() {
+
+  }
+
+  public void shift() {
+
+  }
+
+  public void display() {
+
+  }
+
+  public double getPower() {
+    return power;
+  }
+
+  public double getGrip() {
+    return grip;
+  }
+
+  public double getAero() {
+    return aero;
+  }
+
+  public double getWeight() {
+    return weight;
+  }
+}
